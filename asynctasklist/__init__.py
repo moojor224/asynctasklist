16dc2e165db67afbeab6a88258b1aeb7656d7f48
SpasticHippo224 <64162862+SpasticHippo224@users.noreply.github.com>
2024-06-16 03:01:53 -0500

    code updates

--- asynctasklist/__init__.py
+++ asynctasklist/__init__.py
@@ -1,12 +1,16 @@
 class Task:
-    def __init__(self, init=lambda:(True), task=lambda:(True), id=""):
+    def __init__(self, init=lambda: (True), task=lambda: (True), id=""):
         self.initialized = False
         self.init = init
         self.task = task
         self.id = id
 
+    """Reset the initialized flag to False"""
+
     def reset(self):
         self.initialized = False
+
+    """Run the task"""
 
     def execute(self):
         if not self.initialized:
@@ -16,52 +20,83 @@ class Task:
 
 
 class TaskList:
-    def __init__(self, *tasks):
-        self.tasks = list(tasks)
+    def __init__(self) -> None:
+        self.tasks = []
 
-    def size(self):
+    """Get the number of tasks in the list"""
+
+    def size(self) -> int:
         return len(self.tasks)
 
-    def get(self, index):
+    """Get the task at the specified index in the list"""
+
+    def get(self, index: int) -> Task:
         return self.tasks[index]
 
-    def add(self, task):
-        self.tasks.append(task)
+    """Add a task to the list"""
 
-    def isDone(self):
+    def add(self, task) -> None:
+        if (
+            isinstance(task, Task)
+            or isinstance(task, TaskList)
+            or isinstance(task, ParallelTask)
+        ):
+            self.tasks.append(task)
+
+    """Check if the tasklist is done with executing all tasks"""
+
+    def isDone(self) -> bool:
         return self.size() == 0
 
-    def execute(self):
+    """Execute the next task in the queue"""
+
+    def execute(self) -> bool:
+        # if list is done, return True
         if self.size() == 0:
             return True
-        t = self.tasks[0]
-        isTaskDone = t.execute()
-        if isTaskDone:
+        t = self.tasks[0]  # get the next task
+        isTaskDone = t.execute()  # run it
+        if isTaskDone:  # if the task is done, remove it from the list
             self.tasks.pop(0)
-        return self.isDone()
+        return self.isDone()  # return whether the list is done
+
+    """Clear the tasklist"""
 
     def clear(self):
         self.tasks = []
 
 
-class ParallelTask(Task):
-    def __init__(self, stop_when_done, *tasks):
-        self.list = list(tasks)
+class ParallelTask:
+    def __init__(self, stop_when_done=False):
+        self.tasks = []
         self.stop_when_done = stop_when_done
 
-    def execute(self):
+    """Add a task to the list"""
+
+    def add(self, task):
+        if (
+            isinstance(task, Task)
+            or isinstance(task, TaskList)
+            or isinstance(task, ParallelTask)
+        ):
+            self.tasks.append(task)
+
+    """Execute all tasks in the list"""
+
+    def execute(self) -> bool:
         done = True
         i = 0
-        while i < len(self.list):
-            if not self.list[i].execute():
+        while i < len(self.tasks):
+            if not self.tasks[i].execute():
                 done = False
             else:
                 if self.stop_when_done:
-                    self.list.pop(i)
+                    self.tasks.pop(i)
                     i -= 1
             i += 1
         return done
 
-    def clear(self):
-        self.list = []
+    """Remove all tasks from the list"""
 
+    def clear(self):
+        self.tasks = []
